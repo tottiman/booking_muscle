c18c0c2791b1e5150534788a98f04e28a1e40595
tottiman <toshiki.mmtk@gmail.com>
2020-07-29 21:08:18 +0900

    index
    0729
    index.htma.haml
    bookings.scss
    
    に記述スタート

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root "bookings#index"
   resources :bookings
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
